d0e41257ef4abf9e63996d497e51a81dbdeaed8d
EliJaghab <elijaghab@gmail.com>
2022-07-16 22:57:03 -0400

    add support for edge case

--- soundcloud/tests/spotify_test.py
+++ soundcloud/tests/spotify_test.py
@@ -26,3 +26,18 @@ def test_get_features_unknown_song():
     f = open('tests/expected_responses/expected_unknown.json')
     expected = json.load(f)
     assert expected == actual
+
+def test_get_features_pandemonium():
+    s = Spotify()
+    actual = s.get_features("Pandemonium Tribute Version - Mondays Feat Hanna Stone Dag Lundberg Acoustic Group[Mp3Converter.net]", "Ayush Jain")
+    f = open('tests/expected_responses/expected_unknown.json')
+    expected = json.load(f)
+    assert actual == expected
+
+def test_get_features_memories():
+    s = Spotify()
+    actual = s.get_features("Memories (feat. Kid Cudi) [2021 Remix] (2021 Remix)", "David Guetta")
+    f = open('tests/expected_responses/expected_unknown.json')
+    expected = json.load(f)
+    assert actual == expected
+
