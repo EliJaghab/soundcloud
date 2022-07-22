a1852a22695a47dd1003630d053ce60464f98dfa
EliJaghab <elijaghab@gmail.com>
2022-07-21 23:40:19 -0400

    fix bug in search function

--- soundcloud/utils/spotify.py
+++ soundcloud/utils/spotify.py
@@ -62,9 +62,9 @@ class Spotify():
             def _get_track_id_split_on_hyphen():
                 if "-" in sc_track:
                     split_track = sc_track.rsplit("-", 1)
-                    sc_artist = split_track[0]
-                    sc_track = split_track[1]
-                    return _search_track(sc_track, sc_artist)
+                    sc_artist_split = split_track[0]
+                    sc_track_split = split_track[1]
+                    return _search_track(sc_track_split, sc_artist_split)
                 return None
 
             def _is_valid_response(results):
@@ -81,15 +81,8 @@ class Spotify():
                     track_id_results = _get_track_id_basic()
                 if not _is_valid_response(track_id_results): 
                     track_id_results = _get_track_id_advanced()
-                if not track_id_results:
-                    track_id_results = None, None, None
                 return track_id_results
-            
-            def _is_search_valid(track_id_results):
-                if track_id_results:
-                    return True
-                return False
-            
+
             track_id_results = _perform_search()
             if _is_valid_response(track_id_results):
                 first_result = track_id_results['tracks']['items'][0]
@@ -97,7 +90,7 @@ class Spotify():
                 sp_artist = first_result['artists'][0]['name']
                 sp_track_id = first_result['external_urls']['spotify'][31:]
                 return sp_track, sp_artist, sp_track_id
-            return track_id_results
+            return None, None, None
 
         def _get_features(sp_track: str, sp_artist: str, sp_track_id: str):
             sp_track, sp_artist, sp_track_id = _get_track_id()
